7614cb8ef010767d96ad0e726d2cd7ac6298811f
諸星智也 <panda0909@gmail.com>
2015-06-17 11:47:28 +0900

    decimal

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
     var number : Float = 0
     var number2 : Float = 0
     var operation : Int = 0
-    var result : Int = 0
+    var decimal : Int = 0                //小数点かどうかの判断
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,61 +28,124 @@ class ViewController: UIViewController {
     }
 
     @IBAction func select1(sender: AnyObject) {
-        number = number*10+1.0
-        display(number)
+        if decimal == 1{
+            number = number+0.1*1
+            display2(number)
+        }else{
+        number = number*10+1
+        display(Int(number))
+        }
+        
+        //display(Int(number))
+        
     }
     
     @IBAction func select2(sender: AnyObject) {
-        number = number*10+2.0
-        display(number)
+        if decimal == 1{                     //小数の時の処理
+            number = number+0.1*2
+            display2(number)
+        }else{                               //整数の時の処理
+            number = number*10+2
+            display(Int(number))
+        }
     }
     
     @IBAction func select3(sender: AnyObject) {
-        number = number*10+3.0
-        display(number)
-        
+        if decimal == 1{
+            number = number+0.1*3
+            display2(number)
+        }else{
+            number = number*10+3
+            display(Int(number))
+        }
     }
 
     @IBAction func select4(sender: AnyObject) {
-        number = number*10+4.0
-        display(number)
-        
+        if decimal == 1{
+            number = number+0.1*4
+            display2(number)
+        }else{
+            number = number*10+4
+            display(Int(number))
+        }
     }
     @IBAction func select5(sender: AnyObject) {
-        number = number*10+5.0
-        display(number)
+        if decimal == 1{
+            number = number+0.1*5
+            display2(number)
+        }else{
+            number = number*10+5
+            display(Int(number))
+        }
     }
     
     @IBAction func select6(sender: AnyObject) {
-        number = number*10+6.0
-        display(number)
+        if decimal == 1{
+            number = number+0.1*6
+            display2(number)
+        }else{
+            number = number*10+6
+            display(Int(number))
+        }
     }
     
     @IBAction func select7(sender: AnyObject) {
-        number = number*10+7.0
-        display(number)
+        if decimal == 1{
+            number = number+0.1*7
+            display2(number)
+        }else{
+            number = number*10+7
+            display(Int(number))
+        }
     }
     
     @IBAction func select8(sender: AnyObject) {
-        number = number*10+8.0
-        display(number)
+        if decimal == 1{
+            number = number+0.1*8
+            display2(number)
+        }else{
+            number = number*10+8
+            display(Int(number))
+        }
     }
     
     @IBAction func select9(sender: AnyObject) {
-        number = number*10+9.0
-        display(number)
+        if decimal == 1{
+            number = number+0.1*9
+            display2(number)
+        }else{
+            number = number*10+9
+            display(Int(number))
+        }
     }
     
     @IBAction func select0(sender: AnyObject) {
-        number = number*10+0.0
-        display(number)
+            number = number*10+0
+            display(Int(number))
+
     }
     
     
-    //数字を表示する
-    private func display(number : Float){
+    //数字を表示する(整数)
+    private func display(number : Int){
         label.text = String(stringInterpolationSegment: number)
     }
+    
+    //数字を表示する(少数)
+    private func display2(number : Float){
+        label.text = String(stringInterpolationSegment: number)
+        decimal = 0
+    }
+    
+    
+    //小数点の処理
+    @IBAction func decimal(sender: AnyObject) {
+        label.text = String(stringInterpolationSegment: number)
+        decimal = 1
+
+    }
+    
+    
     
     @IBAction func plus(sender: AnyObject) {
         //plusボタンを押した時に,labelには0が表示される
@@ -131,9 +194,10 @@ class ViewController: UIViewController {
         label.text = String(stringInterpolationSegment: number2 * number)
             number = number2 * number
         }
-        else{
+        else if operation == 4{
         label.text = String(stringInterpolationSegment: number2 / number)
             number = number2 / number
+        }else{
         }
     }
     
@@ -141,6 +205,7 @@ class ViewController: UIViewController {
     @IBAction func clear(sender: AnyObject) {
         number  = 0
         number2 = 0
+        operation = 0
         label.text = String(0)
         
     }
